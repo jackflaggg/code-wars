3057d446abe05ebeae177531438c9818b9dd12e8
jackflaggg <rasul.khamzin@gmail.com>
2025-04-25 21:38:31 +0500

    add 5 task

---
+++ javascript/8-kyu/For UFC Fans.js
@@ -0,0 +1,3 @@
+function quote(fighter) {
+	return fighter.toLowerCase() === 'Conor McGregor' ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" : "I am not impressed by your performance."
+};
