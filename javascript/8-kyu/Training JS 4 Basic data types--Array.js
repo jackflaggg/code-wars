8611aa99aa2067af3372e4897ace26c00ab296ae
jackflaggg <rasul.khamzinnn@gmail.com>
2025-05-08 16:36:48 +0500

    add 112 task

---
+++ javascript/8-kyu/Training JS 4 Basic data types--Array.js
@@ -0,0 +1,22 @@
+function getLength(arr){
+	//return length of arr
+	return arr.length;
+}
+function getFirst(arr){
+	//return the first element of arr
+	return arr.at(0)
+}
+function getLast(arr){
+	//return the last element of arr
+	return arr.at(-1)
+}
+function pushElement(arr){
+	let el=1;
+	//push el to arr
+	arr.push(el)
+	return arr
+}
+function popElement(arr){
+	arr.pop()
+	return arr
+}
